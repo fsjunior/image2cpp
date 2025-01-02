c06c150ebf736ea4908fbe2935c5f91533e25760
Francisco de Souza Junior <fsjunior@gmail.com>
2025-01-02 18:40:39 -0300

    rollback the default dithering mode

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ const settings = {
   drawMode: 'horizontal',
   removeZeroesCommas: false,
   ditheringThreshold: 128,
-  ditheringMode: 3,
+  ditheringMode: 0,
   outputFormat: 'plain',
   invertColors: false,
   rotation: 0,
